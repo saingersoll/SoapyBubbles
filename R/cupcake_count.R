e509e7c744648567cd9a7b37e393fd261bc79db0
saingersoll <sofia.ingersoll@Outlook.com>
2023-08-18 14:28:07 -0700

    Initial commit

---
+++ R/cupcake_count.R
@@ -0,0 +1,10 @@
+cupcake_count <- function(cupcakes, tupperware) {
+  cupcakes * tupperware {
+    if (cupcakes > 12) {
+      print(paste("Whoops, you might need to pick up more tupperware!"))
+  }
+  }
+}
+
+
+
